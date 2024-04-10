4dd30171fcfcb6925af0928a9c3df06d5f000e2d
Ilya <sklarow223@gmail.com>
2024-04-10 12:49:37 +0300

    Author-Domain: Добавляем "порты" на базу данных

---
+++ clean-arc-domain/src/main/java/com/campus/clean/arc/domain/author/port/AuthorIdGenerator.java
@@ -0,0 +1,2 @@
+package com.campus.clean.arc.domain.author.port;public class AuthorIdGenerator {
+}
---
+++ clean-arc-domain/src/main/java/com/campus/clean/arc/domain/author/port/filters/AuthorFilters.java
@@ -0,0 +1,2 @@
+package com.campus.clean.arc.domain.author.port.filters;public class AuthorFilters {
+}
